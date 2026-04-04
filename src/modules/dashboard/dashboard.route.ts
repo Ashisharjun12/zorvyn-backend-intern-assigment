c9c03f03a1480442ce7f072d255359816d14cc8f
Ashisharjun12 <ashishrahul748@gmail.com>
2026-04-04 09:21:00 +0530

    feat:completed dashbaord routes.

--- src/modules/dashboard/dashboard.route.ts
+++ src/modules/dashboard/dashboard.route.ts
@@ -1,12 +1,28 @@
-import {Router} from "express"
+import { Router } from "express";
+import { DashboardRepository } from "./dashboard.repository.js";
+import { DashboardService } from "./dashboard.service.js";
+import { DashboardController } from "./dashboard.controller.js";
+import { authenticate } from "../../middlewares/auth.middleware.js";
+import { customRole } from "../../middlewares/rabc.middleware.js";
+import { validate } from "../../middlewares/validate.middleware.js";
+import { dashboardQuerySchema } from "./dashboard.schema.js";
+
+const router = Router();
 
 
-const router = Router()
+const dashboardRepo = new DashboardRepository();
+const dashboardService = new DashboardService(dashboardRepo);
+const dashboardController = new DashboardController(dashboardService);
 
+// authentication middleware
+router.use(authenticate);
 
-
-
-
-
+// get dashboard summary - (admin, analyst, viewer)
+router.get(
+  "/",
+  customRole("admin", "analyst", "viewer"),
+  validate(dashboardQuerySchema, "query"),
+  dashboardController.getDashboardData
+);
 
 export default router;
